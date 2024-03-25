b5bd272f3abef650d46677d574ffbf6f13035bda
Chahinez125 <chahinez.semmar@yahoo.com>
2024-03-25 14:22:03 +0100

    Ajout du composant Header avec logo et structure de navigationet css logo

---
+++ src/layout/header/Header.jsx
@@ -0,0 +1,26 @@
+import './Header.css'
+import logo from "../../assets/logo.png"
+
+export default function Header(){
+    return<>
+            <header className="header">
+        
+
+        <img className="logo" src={logo} alt='logo'/>
+
+  <nav> 
+     <ul>
+        <li>
+            
+        </li>
+
+    </ul>
+  </nav>
+</header>
+
+
+
+    </> 
+
+
+}
