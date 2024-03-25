dc80a519615954c49332d342b8bfe96c67674160
Chahinez125 <chahinez.semmar@yahoo.com>
2024-03-25 23:50:50 +0100

    import les icon au composant aside

--- src/layout/aside/Aside.jsx
+++ src/layout/aside/Aside.jsx
@@ -1,11 +1,24 @@
-
+import icon1 from "../../assets/icon1.png"
+import icon2 from "../../assets/icon2.png"
+import icon3 from "../../assets/icon3.png"
+import icon4 from "../../assets/icon4.png"
 
 function Aside() {
   return (
     <div>
-      
+      <div>
+      <img src={icon1} alt="icon1" />
+      <img src={icon2} alt="icon2" />
+      <img src={icon3} alt="icon3" />
+      <img src={icon4} alt="icon4" />
+      </div>
+      <div>
+      <p> USESTATE HOOK react permettant à un composant fontionnelle de gérer son etat local en utilisant une variable d état et  fonction pour la mettre a jour </p>
+        
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default Aside
