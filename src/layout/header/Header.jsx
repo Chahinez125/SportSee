49afb29fffbb16dd39e5b4a78a9aefb53481e73f
Chahinez125 <chahinez.semmar@yahoo.com>
2024-03-25 14:34:40 +0100

    header ok

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,26 +1,21 @@
-import './Header.css'
-import logo from "../../assets/logo.png"
+import "./Header.css";
+import logo from "../../assets/logo.png";
 
-export default function Header(){
-    return<>
-            <header className="header">
-        
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <img className="logo" src={logo} alt="logo" />
 
-        <img className="logo" src={logo} alt='logo'/>
-
-  <nav> 
-     <ul>
-        <li>
-            
-        </li>
-
-    </ul>
-  </nav>
-</header>
-
-
-
-    </> 
-
-
+        <nav>
+          <ul>
+            <li>Accueil</li>
+            <li>Profil</li>
+            <li>Réglages</li>
+            <li>Communauté</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
