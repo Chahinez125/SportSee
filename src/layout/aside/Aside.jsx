c9f82429d09787e119854f89ada9f493c69b5bad
Chahinez125 <chahinez.semmar@yahoo.com>
2024-03-26 09:27:27 +0100

    reglage css aside rajouter clanssName icon

--- src/layout/aside/Aside.jsx
+++ src/layout/aside/Aside.jsx
@@ -7,12 +7,12 @@ import "./Aside.css"
 function Aside() {
   return (
     <aside className="aside">
-    <div className="wrap-aside">
-      <div className="wrap-icon">
-      <img src={icon1} alt="icon1" />
-      <img src={icon2} alt="icon2" />
-      <img src={icon3} alt="icon3" />
-      <img src={icon4} alt="icon4" />
+    <div className="wrap-a">
+      <div className="wrap-i">
+      <img className="i-style" src={icon1} alt="icon1" />
+      <img className="i-style" src={icon2} alt="icon2" />
+      <img className="i-style" src={icon3} alt="icon3" />
+      <img className="i-style" src={icon4} alt="icon4" />
       </div>
       
       <span className="copy">Copiryght, SportSee 2020</span>
