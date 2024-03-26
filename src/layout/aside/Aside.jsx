7611b00357d6eaeb1f577626e5fb8f45fe91812d
Chahinez125 <chahinez.semmar@yahoo.com>
2024-03-26 09:15:02 +0100

    maj aside

--- src/layout/aside/Aside.jsx
+++ src/layout/aside/Aside.jsx
@@ -2,22 +2,24 @@ import icon1 from "../../assets/icon1.png"
 import icon2 from "../../assets/icon2.png"
 import icon3 from "../../assets/icon3.png"
 import icon4 from "../../assets/icon4.png"
+import "./Aside.css"
 
 function Aside() {
   return (
-    <div>
-      <div>
+    <aside className="aside">
+    <div className="wrap-aside">
+      <div className="wrap-icon">
       <img src={icon1} alt="icon1" />
       <img src={icon2} alt="icon2" />
       <img src={icon3} alt="icon3" />
       <img src={icon4} alt="icon4" />
       </div>
-      <div>
-      <p> USESTATE HOOK react permettant à un composant fontionnelle de gérer son etat local en utilisant une variable d état et  fonction pour la mettre a jour </p>
-        
+      
+      <span className="copy">Copiryght, SportSee 2020</span>
+ 
 
       </div>
-    </div>
+    </aside>
   );
 }
 
